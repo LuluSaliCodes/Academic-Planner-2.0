6e602978d028c3465b6d8da5cae4d4ca81b49c2d
The-Silent-Orchid <107502113+LuluSaliCodes@users.noreply.github.com>
2026-06-06 20:33:13 +0200

    Trying to add course to array

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,23 @@ const openBtn = document.getElementById("add-course-btn");
 const closeBtn = document.getElementById("close-modal");
 const overlay = document.getElementById("modal-overlay");
 
+const addCourseForm = document.getElementById("add-course-form");
+const courseInput = document.getElementById("course-input");
+
+const courses = [];
+
 openBtn.addEventListener("click", () => {
     overlay.classList.remove("hidden");
 });
 
 closeBtn.addEventListener("click", () => {
     overlay.classList.add("hidden");
+
+    const course = {
+        name: courseInput.textContent.trim()
+    };
+
+    courses.push(course);
 });
+
+console.log(courses);
