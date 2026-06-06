d643a9083cf851ad3c97a3947db26ff1f483042e
The-Silent-Orchid <107502113+LuluSaliCodes@users.noreply.github.com>
2026-06-06 20:10:26 +0200

    Add Course Modal Working

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+const openBtn = document.getElementById("add-course-btn");
+const closeBtn = document.getElementById("close-modal");
+const overlay = document.getElementById("modal-overlay");
+
+openBtn.addEventListener("click", () => {
+    overlay.classList.remove("hidden");
+});
+
+closeBtn.addEventListener("click", () => {
+    overlay.classList.add("hidden");
+});
